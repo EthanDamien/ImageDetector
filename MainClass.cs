2488b194dc6a41fcdd26bc4010aa7f18329fbd1e
Ethan Damien <ethanchang.d@gmail.com>
2021-09-05 22:06:46 -0400

    Commit Name

---
+++ MainClass.cs
@@ -0,0 +1,10 @@
+using System;
+
+class MainClass {
+    public static void Main (string[] args) 
+    {
+        //this will print Hello World
+        Console.WriteLine ("Hello World");
+        
+    }
+}
